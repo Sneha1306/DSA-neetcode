289dddf07a457b6c2053b2edfd87d728d5225f58
Sneha1306 <70309832+Sneha1306@users.noreply.github.com>
2026-09-15 20:57:31 +0530

    Add: anagram-groups - submission-12

---
+++ Data Structures & Algorithms/anagram-groups/submission-12.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>> ana = new HashMap<>();
+
+        for(String s : strs){
+            int[] arr = new int[26];
+            Arrays.fill(arr, 0);
+            for(int i = 0 ; i < s.length() ; i++){
+                arr[s.charAt(i)-'a']++;
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            for(int i = 0 ; i < 26 ; i++){
+                sb.append(arr[i]);
+                sb.append("#");
+            }
+
+            String key = sb.toString();
+
+            if(!ana.containsKey(key)){
+                ana.put(key, new ArrayList());
+            }
+
+            ana.get(key).add(s);
+        }
+
+        return new ArrayList(ana.values());
+
+    }
+}
